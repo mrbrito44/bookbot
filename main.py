f63f9745285dbe6193db8f9af5b393a23aea5349
mrbrito44 <joanbrito23@gmail.com>
2024-10-19 12:14:36 -0500

    created chars_count functions

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    num_words = counter(text)
+    chars_dict = chars_count(text)
+    print(chars_dict)
 
+
+
+def chars_count(text):
+    char_list = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in char_list:
+            char_list[lowered] += 1
+        else:
+            char_list[lowered] = 1
+    return char_list
+
+
+
+
+def counter(text):
+    words = text.split()
+    print(len(words))
+    return len(words)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+
+
+    
+
+        
 
 main()
